d18e36dfdd5893e6f25c26d837145c0202c6a7f8
OxySokolova <poksanaa16@gmail.com>
2023-11-13 21:13:08 -0600

    https

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const[myAdvice, setMyAdvice] = useState(null);
 
     const getAdvice = async() =>{
-      const response = await fetch (`http://www.boredapi.com/api/activity/ `)
+      const response = await fetch (`https://www.boredapi.com/api/activity/ `)
       const data = await response.json();
       setMyAdvice(data.activity);
     }
